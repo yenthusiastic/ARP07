825f1774c27a7a268d137f44732e16f70350487b
andy.markwart <andy.markwart@gmail.com>
2019-06-18 21:08:32 +0200

    find deltas, fix deltas

--- code/andy/auto-copy.py
+++ code/andy/auto-copy.py
@@ -1,6 +1,7 @@
 import sys
 import os
 from subprocess import PIPE, run
+import shutil
 
 
 std_disks = ['internal_0', 'rootfs', 'boot']
@@ -8,22 +9,22 @@ media_path = "/media/andy/"
 path_internal = media_path + "internal_0"
 path_disk_label = "/dev/disk/by-label"
 
+path_internal_dev = "/home/andy/projects/1/data/"
+path_external_dev = "/home/andy/projects/2/"
+
 
 def cmd(command):
     result = run(command, stdout=PIPE, stderr=PIPE, universal_newlines=True, shell=True)
     return result.stdout
 
 
-
-
-disks_label = os.listdir(path_disk_label)
-
 #print(os.chroot('/media/andy/internal_0'))
 #print(os.listdir(path_internal))
 #print(os.listdir(path_disk_label))
 
 
-def check_disks():
+def check_disks(path=path_disk_label):
+    disks_label = os.listdir(path)
     found = []
     for disk in disks_label:
         if disk not in std_disks:
@@ -31,13 +32,46 @@ def check_disks():
     return found
 
 
-# TODO: check directory first level of external disk and data directory of internal disk for deltas
-# TODO: copy missing session-folders from internal to external
-# TODO: bonus: check external for code-updates and overwrite internal
+# TODO: finalize disk path routine
+# TODO: check external for code-updates and overwrite internal
 
-ex_ls = check_disks()
+"""
+ex_ls = check_disks(path_disk_label)
 if ex_ls:
     print(ex_ls)
     ls_lvl_1 = os.listdir(media_path + ex_ls[0])
     print(ls_lvl_1)
+"""
 
+
+def get_file_delta(path_source, path_target):
+    files_delta = []
+    files_source = os.listdir(path_source)
+    files_target = os.listdir(path_target)
+    for file in files_source:
+        if file not in files_target:
+            files_delta.append(file)
+    return files_delta
+
+
+# copy files from path_source to path_target
+# or copy file path_source to path_target
+def copy_files(path_source, path_target, files=None):
+    if files:
+        for file in files:
+            #shutil.copy2(path_source+str(file), path_target+str(file))
+            shutil.copytree(path_source + str(file), path_target + str(file))
+    else:
+        pass
+        #shutil.copy2(path_source, path_target)
+    print("copy: ", files)
+
+
+def fix_file_deltas(path_source, path_target):
+    deltas = get_file_delta(path_source, path_target)
+    copy_files(path_source, path_target, deltas)
+
+
+if get_file_delta(path_internal_dev, path_external_dev):
+    print("found deltas!")
+    fix_file_deltas(path_internal_dev, path_external_dev)
